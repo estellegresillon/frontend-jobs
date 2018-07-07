f2687a9074e1a786b89e99fa47bbaefbbac09be9
estellegresillon <estelle.gresillon@gmail.com>
2018-07-07 16:20:02 +0200

    css

--- lvl3/lvl3.js
+++ lvl3/lvl3.js
@@ -6,6 +6,7 @@ const downloadMe = document.getElementById('downloadMe');
 const findTotal = document.getElementById('findTotal');
 const btn = document.getElementById('btn');
 
+
 fetch(`http://hapi.fhir.org/baseDstu3/Binary`)
   .then(response => response.json())
   .then((data) => {
@@ -25,18 +26,21 @@ dropZone.addEventListener('drop', function(e) {
 
     const fileName = files[0].name;
 
+    fileNameInput.innerHTML = ""
     fileNameInput.insertAdjacentHTML('beforeend', `${fileName}`);
 
     fetch('https://fhirtest.uhn.ca/baseDstu3/Binary', { method: 'POST', body: files[0] })
       .then(response => response.json())
       .then((data) => {
-        fileUploaded.insertAdjacentHTML('beforeend', "- File created !")
+        fileUploaded.innerHTML = ""
+        fileUploaded.insertAdjacentHTML('beforeend', " - <strong>File created !</strong>")
          fetch(`http://hapi.fhir.org/baseDstu3/Binary`)
           .then(response => response.json())
           .then((data) => {
+            downloadMe.innerHTML = ""
             downloadMe.insertAdjacentHTML('beforeend',
               `<form action="https://fhirtest.uhn.ca/baseDstu3/Binary/${data.entry[0].resource.id}/_history/1">
-                <input type="submit" value="Download me !" />
+                <input type="submit" value="Download file" />
               </form>`)
             findTotal.innerHTML = ""
             findTotal.insertAdjacentHTML('beforeend', `${data.total}`);
@@ -45,16 +49,18 @@ dropZone.addEventListener('drop', function(e) {
 
       for (var i=0, file; file=files[i]; i++) {
         if (file.type.match(/image.*/)) {
-            var reader = new FileReader();
+          var reader = new FileReader();
 
-            reader.onload = function(e2) {
-                // finished reading file data.
-                var img = document.createElement('img');
-                img.src= e2.target.result;
-                document.body.appendChild(img);
-            }
+          reader.onload = function(e2) {
+            // finished reading file data.
+            const img = document.createElement('img');
+            const displayImg = document.getElementById('displayImg');
+            img.src = e2.target.result;
+            displayImg.innerHTML = ""
+            displayImg.appendChild(img);
+          }
 
-            reader.readAsDataURL(file); // start reading the file data.
+          reader.readAsDataURL(file); // start reading the file data.
         }
     }
 
